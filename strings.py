bca640238fb8ac62f40155089fc135cd6f15bac9
Marcos Fellipe Guimarães <85682890+Markfgui675@users.noreply.github.com>
2022-10-22 15:20:00 -0300

    Strings

--- strings.py
+++ strings.py
@@ -1,20 +1,39 @@
 def disemvowel(string_):
     string = []
+    string1 = []
+    query = ''
     for c in range(0, len(string_)):
         string.append(string_[c])
+        string1.append(string_[c])
     
     for i in range(len(string)):
         if string[i] == 'a':
-            string.pop(i)
+            string1.remove('a')
+        if string[i] == 'A':
+            string1.remove('A')
         if string[i] == 'e':
-            string.pop(i)
+            string1.remove('e')
+        if string[i] == 'E':
+            string1.remove('E')
         if string[i] == 'i':
-            string.pop(i)
+            string1.remove('i')
+        if string[i] == 'I':
+            string1.remove('I')
         if string[i] == 'o':
-            string.pop(i)
+            string1.remove('o')
+        if string[i] == 'O':
+            string1.remove('O')
         if string[i] == 'u':
-            string.pop(i)
+            string1.remove('u')
+        if string[i] == 'U':
+            string1.remove('U')
+    
+    for z in range(0, len(string1)):
+        if z == 0:
+            query = string1[z]
+        else:
+            query += string1[z]
 
-    return len(string)
+    return query
 
-print(disemvowel('This website is for losers LOL!'))
+print(disemvowel("This website is for losers LOL!"))
